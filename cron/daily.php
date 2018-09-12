4c0ac735eb691612c16d18d9ecc8cc3996f67839
mctrivia <squarerootofnegativeone@gmail.com>
2018-09-11 19:12:37 -0500

    fixed relative addressing problem

--- cron/daily.php
+++ cron/daily.php
@@ -1,7 +1,7 @@
 <?php
 	
-	require_once '../includes/autoload.php';
-	require_once '../includes/CryptoAuth.php';
+	require_once __DIR__.'/../includes/autoload.php';
+	require_once __DIR__.'/../includes/CryptoAuth.php';
 	
 	
 	
