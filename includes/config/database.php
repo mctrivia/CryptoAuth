654d072b49e77480f4b51402ebf92920019a5e5a
mctrivia <squarerootofnegativeone@gmail.com>
2018-09-12 00:44:19 -0500

    created by admin/setup.php now

--- includes/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'auth2');
-define('DB_PASS', 'cHGnahehYTMqQhGb');		//use your own password	
-define('DB_DATA', 'auth2');
-
-define('MASTER_SERVER',	'http://auth.digibyte.rocks/api/');
